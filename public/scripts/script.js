48bdfdf99665aa2037a30d5facca62f77456f9b9
Erin Black <erinblackdesign@gmail.com>
2017-06-01 14:54:47 -0500

    Got the drop down to populate with owners

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,6 +3,7 @@ $(onReady);
 function onReady() {
   $('#ownerRegister').on('click', registerOwner);
   $('#addPet').on('click', addPet);
+  ownerSelect();
 }
 
 
@@ -37,6 +38,25 @@ function registerOwner(){
     }
   });
 } // end registerOwner function
+
+function ownerSelect() {
+  $.ajax({
+    type: 'GET',
+    url: '/addOwner',
+    success: function(response){
+    console.log('back from server with good news!', response);
+    for (var i = 0; i < response.length; i++) {
+      var $option = $('<option value="' + response[i].first_name + ' ' +
+      response[i].last_name + '">' + response[i].first_name + ' ' +
+      response[i].last_name +' </option>'  );
+
+      $("#ownerSelect").append($option);
+    }
+  }//end of succes
+  });//end of ajax
+
+}
+
 
 // function addPet(){
 //   console.log('enter pet registration');

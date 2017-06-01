ddfc6d013c80dde1ebe5410ba501a083f0064303
Erin Black <erinblackdesign@gmail.com>
2017-06-01 14:24:50 -0500

    Got info from server

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,15 +19,24 @@ function registerOwner(){
   console.log(objectToSend);
 
   $.ajax({
-type: 'POST',
-url: '/addOwner',
-data: objectToSend,
-success: function(response) {
+    type: 'POST',
+    url: '/addOwner',
+    data: objectToSend,
+    success: function(response) {
   console.log('back from the server with response', response);
-}
+}// end success
 
+  });// end ajax call
+
+  $.ajax({
+    type: 'GET',
+    url: '/addOwner',
+    success: function(response){
+    console.log('back from server with good news!', response);
+
+    }
   });
-}
+} // end registerOwner function
 
 // function addPet(){
 //   console.log('enter pet registration');
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 
-var allOwners = [];
 var port = 3000;
 var config = {
 database: 'pet_hotel',
@@ -21,14 +20,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 //listen
 app.listen(port, function(){
   console.log('server up on 3000');
-});
+});//end listen
 
 app.get('/', function(req, res){
   console.log('url hit ');
   res.sendFile(path.resolve('public/views/index.html'));
-});
+}); //end get
 
+
+// posting owner info to database on register owner function
 app.post('/addOwner', function (req, res){
+  var allOwners = [];
   console.log('post hit on /addOwner');
 allOwners.push(req.body);
 console.log('allOwners', allOwners);
@@ -38,20 +40,38 @@ pool.connect( function(err, connection, done){
     console.log(err);
     done();
     res.send(400);
-  }
+  }// end if
 else {
   console.log('connected to db');
   connection.query("INSERT INTO user_info (first_name, last_name) VALUES ('" + req.body.firstName + "'  , '" + req.body.lastName + "' )");
   done();
   res.send(200);
-}
+}// end else
 
-});
+}); //end pool connect
 
+});// end post addOwner
 
-
-
-
-
-
-});
+// getting all owners from the db
+app.get('/addOwner', function(req, res){
+  var allOwners = [];
+  console.log('executed addOwner get');
+  pool.connect( function(err, connection, done){
+    if( err ){
+      console.log(err);
+      done();
+      res.send(400);
+    }// end if
+  else {
+    console.log('connected to db');
+    var resultSet = connection.query("SELECT * FROM user_info");
+    resultSet.on('row', function(row){
+      allOwners.push(row);
+    }); //end
+    resultSet.on('end', function(){
+      done();
+      res.send(allOwners);
+    }); //end
+  }// end else
+  }); //end pool connect
+}); //end get
